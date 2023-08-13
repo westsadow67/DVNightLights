673fe8b9294f0c1b1a4ac16b30013d308baa7250
westsadow67 <60273918+westsadow67@users.noreply.github.com>
2023-08-13 15:59:15 -0500

    folder added

---
+++ CUBE/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityModManagerNet;
+using UnityEngine;
+
+namespace CUBE
+{
+    public class Main
+    {
+        public static UnityModManager.ModEntry myModEntry;
+        public static string ModEntryPath;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            myModEntry = modEntry;
+            ModEntryPath = myModEntry.Path;
+
+            WorldStreamingInit.LoadingFinished += CubeInit;
+
+            return true;
+        }
+
+        private static void CubeInit()
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(0, 130, 0);
+            cube.transform.localScale = new Vector3(10, 10, 10);
+        }
+    }
+}
